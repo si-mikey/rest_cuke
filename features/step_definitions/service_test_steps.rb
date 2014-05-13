667d764cb2b73b19d14296fe93193bf7582d2fcf
Luis <mikeylopez@gmail.com>
2014-05-13 01:13:03 -0400

    steps

---
+++ features/step_definitions/service_test_steps.rb
@@ -0,0 +1,19 @@
+Given(/^I setup the "(.*?)" service$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I make a "(.*?)" call$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the service returns the status "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the service returns the content\-type "(.*?)"application\/json; charset=utf\-(\d+)"(.*?)"$/) do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the service returns the body "(.*?)"This is the (\d+) response"(.*?)"$/) do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
